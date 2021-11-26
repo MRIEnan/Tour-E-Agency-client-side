efcb1fae626801fbd95b2dcdbde9ebdde12eb5b0
mainur rahaman <enan.mainur@gmail.com>
2021-11-27 05:53:26 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAf1Nd43Bg51v5ObCU6ZxgIngmfzePH-HQ",
-    authDomain: "tour-e-e370e.firebaseapp.com",
-    projectId: "tour-e-e370e",
-    storageBucket: "tour-e-e370e.appspot.com",
-    messagingSenderId: "488042342890",
-    appId: "1:488042342890:web:b65584637a17a44c846a93"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
 export default firebaseConfig;
